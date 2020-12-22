b12210354b7ccc396abc0839cedafa541e830aa5
uli <uli@plabst.org>
2020-12-22 12:43:35 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-455d1f9656c7e28e79d9.js"
+    "url": "webpack-runtime-cb2e9bd530ba651d908e.js"
   },
   {
     "url": "framework-fd9b1329047fdaf03907.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "529ee1d32c24f1a87f3908080e7e9291"
+    "revision": "6e34061aecec350a98080242af317e1b"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -47,7 +47,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e0a08a7479867bfc8bab3d06b6966c95"
+    "revision": "0c5c7848d3a0e676c6c9810b55f74978"
   },
   {
     "url": "polyfill-11b7260e953fe0a3ae2a.js"
